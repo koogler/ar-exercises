64f99c54c602dd2f46e7d59cd6de41a63eae1003
Desmond Parker <evan.parker95@gmail.com>
2022-02-01 02:29:34 +0000

    completion of first three exercises

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,4 +6,26 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-# Your code goes here ...
+store = Store.create(
+  name: "Surrey",
+  annual_revenue: 224000,
+  mens_apparel: false,
+  womens_apparel: true 
+)
+
+store = Store.create(
+  name: "Whistler",
+  annual_revenue: 1900000,
+  mens_apparel: true,
+  womens_apparel: false 
+)
+
+store = Store.create(
+  name: "Yaletown",
+  annual_revenue: 430000,
+  mens_apparel: true,
+  womens_apparel: true
+)
+
+@mens_stores = Store.find_by(mens_apparel: true)
+
